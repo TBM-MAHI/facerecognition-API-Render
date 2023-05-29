0dea8e6edef6d6b1ab0433a43b620b454a645198
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2023-05-29 21:38:25 +0600

    mo

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
+function connectionToDB() {
+  const knex = require("knex")({
+    client: "pg",
+    connection:
+      "postgres://mahi:7nfoNMtE0XZRJubfeSV4q8kOFmNyDZgV@dpg-chd1mrl269vdj68g8e70-a.oregon-postgres.render.com/facerecognition_mkxq",
+  });
 
-
-function connectionToDB(){
-const knex = require("knex")({
-  client: "pg",
-  connection:
-    "postgres://mahi:7nfoNMtE0XZRJubfeSV4q8kOFmNyDZgV@dpg-chd1mrl269vdj68g8e70-a.oregon-postgres.render.com/facerecognition_mkxq",
-});
-    
-console.log(knex)
+  knex
+    .raw("SELECT 1")
+    .then(() => {
+      console.log("Database connection successful");
+    })
+    .catch((error) => {
+      console.error("Database connection failed:", error);
+    });
 }
 
-module.exports = connectionToDB
+module.exports = connectionToDB;
